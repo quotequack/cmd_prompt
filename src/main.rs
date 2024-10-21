a69f055862074345775488dc89a9fc710a538e6e
QuoteQuack <118599418+quotequack@users.noreply.github.com>
2024-10-21 19:33:12 +0300

    added theme + extra cmds

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use fltk::{app, input::Input, prelude::*, window::Window};
 use rdev::{listen, EventType, Key as keyer, Event};
 use std::process::{exit, Command};
+use fltk_theme::{ColorTheme, color_themes};
 
 fn main() {
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
+    let theme = ColorTheme::new(color_themes::BLACK_THEME);
+    theme.apply();
     let mut wind = Window::new(100, 100, 150, 100, "Enter Command");
     let input = Input::new(0, 0, 150, 100, "");
 
@@ -38,6 +41,10 @@ fn process_input(input: &str) {
         "exit;" => println!("Exiting..."),
         "quack;" => quack(true),
         "quack 0;" => quack(false),
+        "paste;" => paste(true),
+        "paste 0;" => paste(false),
+        "typer;" => typer(true),
+        "typer 0;" => typer(false),
         _ => println!("Unrecognized command: {}", input),
     }
 }
@@ -47,5 +54,22 @@ fn quack(arg: bool) {
         Command::new("quack").arg("&").spawn().expect("quack fail");
     } else {
         Command::new("pkill").arg("quack").spawn().expect("shotgun miss");
+    }
+}
+
+fn paste(arg: bool) {
+    if arg {
+        Command::new("forcepaste").arg("&").spawn().expect("paste fail");
+    } else {
+        Command::new("pkill").arg("forcepaste").spawn().expect("shotgun miss");
+    }
+}
+
+
+fn typer(arg: bool) {
+    if arg {
+        Command::new("rustytyper").arg("&").spawn().expect("typer fail");
+    } else {
+        Command::new("pkill").arg("rustytyper").spawn().expect("shotgun miss");
     }
 }
